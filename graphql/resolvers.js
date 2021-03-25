3de202be38abec11385a6a3faaf73098009ff7d9
Joooohee <joooohee@kakao.com>
2021-03-25 15:00:59 +0900

    Add API

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from "../db";
+import { getMovie, getMovies, getSuggestions } from "../db";
 
 const resolvers = {
   Query: {
     movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
   },
 };
 
